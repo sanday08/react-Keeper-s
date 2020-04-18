effe4506eaf7415e6db637b1fd7c3a135a41d5d6
sanday08 <sandip.job89@gmail.com>
2020-04-18 22:08:38 +0530

    Updates

---
+++ precache-manifest.21783ba9df4f767e2e7d604e4e008dc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86daca8b6c70fa4e3a88c035a97667bd",
+    "url": "/sanday08/react-Keeper-s.git/index.html"
+  },
+  {
+    "revision": "8a58b224f6d83e5981fa",
+    "url": "/sanday08/react-Keeper-s.git/static/js/2.62475452.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/sanday08/react-Keeper-s.git/static/js/2.62475452.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4683ae7640fe954efe4f",
+    "url": "/sanday08/react-Keeper-s.git/static/js/main.57ad6c4d.chunk.js"
+  },
+  {
+    "revision": "8d965f4b3fdbfa8c4906",
+    "url": "/sanday08/react-Keeper-s.git/static/js/runtime-main.b58d6fd3.js"
+  }
+]);
